bf89f639551d67edf2af8ec19d8d9f8abe694ac1
fadhluibnu <f.ibnu.a1@gmail.com>
2021-11-19 10:23:14 +0700

    Assets

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Hallo, nama saya <?php echo $data['nama'] ?>, umur saya <?php echo $data['umur'] ?>, pekerjaan saya <?php echo $data['pekerjaan'] ?> </p>
+<div class="container">
+    <h1>About Me</h1>
+    <img src="<?php echo BASEURL; ?>/img/fadhluabbadght.png" alt="Fadhlu Ibnu" width="200" class="rounded-circle shadow" height="200">
+    <p>Hallo, nama saya <?php echo $data['nama'] ?>, umur saya <?php echo $data['umur'] ?>, pekerjaan saya <?php echo $data['pekerjaan'] ?> </p>
+</div>
--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,3 +1,4 @@
+<script src="<?php echo BASEURL ?>/js/bootstrap.js"></script>
 </body>
 
 </html>
